41e19844bdf79d1b512d175efc7832b8de41ed1c
Chuan Zheng <zhengchuan000@gmail.com>
2023-05-17 22:26:34 +0800

    project init

---
+++ dags/consumer.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow import Dataset
+from airflow.decorators import task
+
+my_file = Dataset("/tmp/my_file.txt")
+
+with DAG(
+        dag_id="consumer",
+        schedule=[my_file],
+        start_date=datetime(2022, 1, 1),
+        catchup=False
+):
+    @task
+    def read_dataset():
+        with open(my_file.uri, "r") as f:
+            print(f.read())
+
+
+    read_dataset
